d764f245214622a3c8da7feb1b4323266fba15de
Felix Pusch <mail@felix-pusch.com>
2021-05-16 11:28:30 +0200

    Add haskell for treesitter

--- vim/vim.symlink/lua/fpusch/treesitter.lua
+++ vim/vim.symlink/lua/fpusch/treesitter.lua
@@ -1,6 +1,6 @@
 -- treesitter configuration
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "bash", "c", "cpp" , "css", "go", "graphql", "html", "java", "javascript", "json", "latex", "regex", "rust", "typescript", "yaml" } ,
+    ensure_installed = { "bash", "c", "cpp" , "css", "go", "graphql", "haskell", "html", "java", "javascript", "json", "latex", "regex", "rust", "typescript", "yaml" } ,
     ignore_install = { },
     highlight = {
         enable = true,
