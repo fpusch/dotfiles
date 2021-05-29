91b59c8a3c0ee256fb84abdc6455a25631339a86
Felix Pusch <mail@felix-pusch.com>
2021-05-29 12:11:33 +0200

    Add Haskell LSP

--- vim/vim.symlink/lua/fpusch/lsp.lua
+++ vim/vim.symlink/lua/fpusch/lsp.lua
@@ -1,8 +1,6 @@
--- lsp server setup goes here
-local function on_attach()
-    -- dummy on_attach for now
-end
-
 -- languages
 -- typescript (run `npm i -g typescript typescript-language-server`)
 require'lspconfig'.tsserver.setup{}
+
+-- haskell (requires haskell-language-server from anywhere)
+require'lspconfig'.hls.setup{}
