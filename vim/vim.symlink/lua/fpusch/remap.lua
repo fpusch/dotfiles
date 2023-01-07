62e0d97dd5eb1b52921b0bf679456937ee614c22
Felix Pusch <mail@felix-pusch.com>
2023-01-07 16:17:58 +0100

    fix terminal mode escape

--- vim/vim.symlink/lua/fpusch/remap.lua
+++ vim/vim.symlink/lua/fpusch/remap.lua
@@ -17,6 +17,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("t",  "<Esc>", "<C-\\><C-n>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
