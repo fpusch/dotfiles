4827812383e89c1273c3098a14b1e5c811b6b004
Felix Pusch <mail@felix-pusch.com>
2026-03-08 17:33:03 +0100

    update/remove lsp stuff

--- vim/vim.symlink/lua/fpusch/packer.lua
+++ vim/vim.symlink/lua/fpusch/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        'nvim-telescope/telescope.nvim', tag = 'v0.2.1',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
@@ -20,29 +20,5 @@ return require('packer').startup(function(use)
     }
 
     use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
-    use('mbbill/undotree')
     use('tpope/vim-fugitive')
-
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
-
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
-        }
-    }
-
 end)
